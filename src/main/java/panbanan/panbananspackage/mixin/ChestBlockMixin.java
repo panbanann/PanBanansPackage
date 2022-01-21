2a7a4e120c3ea883363569d2c11e92665cc6ddda
panbanann <pan.banan@gmail.com>
2022-01-21 21:06:43 +0000

    Mimic spawn with less than 10% chance on chest oppening. Works only for chest that were unopened.

--- src/main/java/panbanan/panbananspackage/mixin/ChestBlockMixin.java
+++ src/main/java/panbanan/panbananspackage/mixin/ChestBlockMixin.java
@@ -2,7 +2,7 @@ package panbanan.panbananspackage.mixin;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ChestBlock;
-import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
@@ -13,27 +13,30 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import panbanan.panbananspackage.client.LootIdObtainer;
 import panbanan.panbananspackage.entity.EntityRegister;
 import panbanan.panbananspackage.entity.mobs.MimicEntity;
 
-import javax.annotation.Nullable;
+import java.util.Random;
 
 @Mixin(ChestBlock.class)
 public abstract class ChestBlockMixin {
-    //public Random rand = new Random();
+    public Random rand = new Random();
 
     @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
     public void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-        //int int_random = rand.nextInt(100);
-        System.out.println("The block was used.");
-
-        if (LootIdObtainer.ObtainLootId() != null) {
-            System.out.println("ChestLootId wasn't null.");
+        int int_random = rand.nextInt(100);
+        //System.out.println("The block was used.");
+        BlockEntity chestBlockEntity = world.getBlockEntity(pos);
+        LootableContainerBlockEntityAccessor tableAccess = ((LootableContainerBlockEntityAccessor) chestBlockEntity);
+        @Nullable
+        Identifier lootTableId = tableAccess.getLootTableId();
+        if (lootTableId != null && int_random<=9) {
+            //System.out.println("ChestLootId wasn't null.");
             AreaEffectCloudEntity poofCloud = new AreaEffectCloudEntity(world, pos.getX(), pos.getY(), pos.getZ());
             player.playSound(SoundEvents.BLOCK_CHEST_OPEN, 1.0F, 0.5F);
             poofCloud.setRadius(10.0F);
@@ -49,10 +52,7 @@ public abstract class ChestBlockMixin {
                 world.removeBlock(pos, false);
             }
 
-        }else {
-            System.out.println("ChestLootId is null null.");
         }
-
 
     }
 }
